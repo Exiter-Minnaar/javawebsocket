584c566d58294b4b20c39adcb458ab6ef02f8b15
Cornelius <reinhardcorneluisminnaar@email.com>
2026-06-22 20:18:12 +0200

    Comment documentry.

--- CLIENT/client/src/main/java/org/root/Client.java
+++ CLIENT/client/src/main/java/org/root/Client.java
@@ -4,33 +4,39 @@ import java.net.URI;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
+//WebSocketClient is an abstract class.
 class WsClient extends WebSocketClient {
     public WsClient(URI serverURI) {
         super(serverURI);
     }
     
+    //Fires when a servee connects.
     @Override
     public void onOpen(ServerHandshake handshake) {
         System.out.println("Connected!");
         send("Hi from client!");
     }
     
+    //Fires when the server sends data.
     @Override
     public void onMessage(String message) {
         System.out.println("From server: %s".formatted(message));
     }
     
+    //Fires when the server disconnects.
     @Override
     public void onClose(int code, String reason, boolean remote) {
         System.out.println("Disconnected!");
     }
     
+    //Handles possible connection failure.
     @Override
     public void onError(Exception exception) {
         exception.printStackTrace();
     }
 }
 
+//Nain java entry point.
 public class Client {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Client!");
--- SERVER/server/src/main/java/org/root/Server.java
+++ SERVER/server/src/main/java/org/root/Server.java
@@ -5,38 +5,46 @@ import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
+//WebSocketServer is an abstract class.
 class WsServer extends WebSocketServer {
     public WsServer(int port) {
+        //Access the parent class parameter.
         super(new InetSocketAddress(port));
     }
     
+    //Fires when a client connects.
     @Override
     public void onOpen(WebSocket connection, ClientHandshake handshake) {
         System.out.println("Connected, address: %s".formatted(connection.getRemoteSocketAddress()));
     }
     
+    //Fires when a client disconnects.
     @Override
     public void onClose(WebSocket connection, int code, String reason, boolean remote) {
         System.out.println("Server disconnected!");
     }
     
+    //Fires when the client sends data.
     @Override
     public void onMessage(WebSocket connection, String message) {
         System.out.println("Recieved: %s".formatted(message));
         connection.send("Server: Recieved message, %s".formatted(message));
     }
     
+    //If an error occurs this code handles it.
     @Override
     public void onError(WebSocket connection, Exception exception) {
         exception.printStackTrace();
     }
     
+    //Fires when the server is activated.
     @Override
     public void onStart() {
         System.out.println("Listening on port: 6890");
     }
 }
 
+//Main java entry point.
 public class Server {
     public static void main(String[] args) {
         System.out.println("Hello, Server!");
